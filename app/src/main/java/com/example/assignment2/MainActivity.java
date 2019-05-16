5b641ec2553c44554365cc0099a768ab3bb28850
ryan wong <ryanziming.wong@my.jcu.edu.au>
2019-05-16 18:43:51 +0800

    Added answer cards

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Defining UI elements
+    private ImageView bgImage, centerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Identifying image views, and buttons
+        bgImage = findViewById(R.id.BgImg);
+        centerImage = findViewById(R.id.CenterImg);
     }
 }
